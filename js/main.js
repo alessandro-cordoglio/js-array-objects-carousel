2edaf2150df5a41701111cafc5b34ef4af76f3c4
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-11-09 18:05:40 +0100

    piccoli fix riguardanti le classi

--- js/main.js
+++ js/main.js
@@ -13,26 +13,32 @@ Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi)
 BONUS 3:
 Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
  */
-
+const BtnNext= document.querySelector(".next")
+const BtnPrev= document.querySelector(".previous")
 
 const images = [
     {
+        id: 1,
         image: 'img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
+        id: 2,
         image: 'img/02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     }, {
+        id: 3,
         image: 'img/03.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     }, {
+        id: 4,
         image: 'img/04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
     }, {
+        id: 5,
         image: 'img/05.webp',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
@@ -43,8 +49,10 @@ images.forEach(element => {
     const container= document.querySelector(".items-list")
     const itemElement= document.createElement("div")
     itemElement.classList.add("item-element")
-    const innerElement= `<img src="${element.image}" alt="">`
+    const innerElement= `<img id="${element.id}" src="${element.image}" alt="">`
     itemElement.innerHTML=innerElement
     container.append(itemElement)
-    console.log()
 });
+/* BtnNext.addEventListener("click", function{
+
+}) */
